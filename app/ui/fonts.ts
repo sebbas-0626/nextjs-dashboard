6ad359481dc5c261862ba6e863c7185af33d860c
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-12-12 23:55:26 -0500

    feat: :sparkles: agregar archivo de fuentes Lusitana

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
+
+export default lusitana;
